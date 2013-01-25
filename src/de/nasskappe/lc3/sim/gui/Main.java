4a629f091af5c78ccf3a83aba99138f3c15626b8
Tobias Mayer <github@tobiasm.de>
2013-01-24 18:26:30 -0800

    Default to Nimbus Look&Feel

--- src/de/nasskappe/lc3/sim/gui/Main.java
+++ src/de/nasskappe/lc3/sim/gui/Main.java
@@ -1,13 +1,23 @@
 package de.nasskappe.lc3.sim.gui;
 
 import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
 import javax.swing.UnsupportedLookAndFeelException;
 
 public class Main {
 	
 	
 	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
-		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		try {
+			for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+		        if ("Nimbus".equals(info.getName())) {
+		            UIManager.setLookAndFeel(info.getClassName());
+		            break;
+		        }
+		    }
+		} catch (Exception e) {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		}
 		
 		MainWindow window = new MainWindow();
 		
